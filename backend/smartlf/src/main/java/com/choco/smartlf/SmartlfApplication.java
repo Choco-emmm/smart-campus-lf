f5ec51deb1c14a3cfc1daa51cb8650d8c9e10d64
Choco-emmm <renpei233@gmail.com>
2026-04-05 23:30:15 +0800

    第一天

---
+++ backend/smartlf/src/main/java/com/choco/smartlf/SmartlfApplication.java
@@ -0,0 +1,14 @@
+package com.choco.smartlf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartlfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartlfApplication.class, args);
+    }
+
+}
+    
---
+++ backend/smartlf/src/test/java/com/choco/smartlf/SmartlfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.choco.smartlf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartlfApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
